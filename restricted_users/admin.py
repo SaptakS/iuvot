b18cceb645c0bdc33b6b0325d18d8d6fc7bc32cc
Saptak S <saptak013@gmail.com>
2022-08-14 22:18:49 +0530

    Restricts number of entries in the User model

--- restricted_users/admin.py
+++ restricted_users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
+from restricted_users.models import User
 
+
+admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
--- restricted_users/models.py
+++ restricted_users/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 
 
+class DbLimitException(BaseException):
+      pass
+
 class User(AbstractUser):
-    pass
+    def save(self, *args, **kwargs):
+        if User.objects.count() >= 1:
+            raise DbLimitException({"message": "Db limit reached please delete to add more data"})
+        else:
+            super().save(*args,**kwargs)
